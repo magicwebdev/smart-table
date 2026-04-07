ca7abfe7831d38ce9b57364bb1226b98a66b21fb
Александра Храпкова <magicwebdev@yandex.ru>
2026-04-07 16:43:09 +0300

    добавляет шаг 2

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import {processFormData} from "./lib/utils.js";
 
 import {initTable} from "./components/table.js";
 // @todo: подключение
+import { initPagination } from './components/pagination.js';
 
 
 // Исходные данные используемые в render()
@@ -19,9 +20,12 @@ const {data, ...indexes} = initData(sourceData);
  */
 function collectState() {
     const state = processFormData(new FormData(sampleTable.container));
-
+    const rowsPerPage = parseInt(state.rowsPerPage);
+    const page = parseInt(state.page ?? 1);
     return {
-        ...state
+        ...state,
+        rowsPerPage,
+        page
     };
 }
 
@@ -30,11 +34,10 @@ function collectState() {
  * @param {HTMLButtonElement?} action
  */
 function render(action) {
-    let state = collectState(); // состояние полей из таблицы
-    let result = [...data]; // копируем для последующего изменения
+    let state = collectState(); 
+    let result = [...data]; 
     // @todo: использование
-
-
+    result = applyPagination(result, state, action); 
     sampleTable.render(result)
 }
 
@@ -42,11 +45,21 @@ const sampleTable = initTable({
     tableTemplate: 'table',
     rowTemplate: 'row',
     before: [],
-    after: []
+    after: ['pagination']
 }, render);
 
 // @todo: инициализация
-
+const applyPagination = initPagination(
+    sampleTable.pagination.elements,
+    (el, page, isCurrent) => {
+        const input = el.querySelector('input');
+        const label = el.querySelector('span');
+        input.value = page;
+        input.checked = isCurrent;
+        label.textContent = page;
+        return el;
+    }
+);
 
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
