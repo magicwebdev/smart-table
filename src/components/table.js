cc0718a84b6d9a699f6738b9213d1b5a297c6bf6
Александра Храпкова <magicwebdev@yandex.ru>
2026-04-07 15:06:06 +0300

    добавляет шаг 1

--- src/components/table.js
+++ src/components/table.js
@@ -11,13 +11,39 @@ export function initTable(settings, onAction) {
     const {tableTemplate, rowTemplate, before, after} = settings;
     const root = cloneTemplate(tableTemplate);
 
-    // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    // вывод дополнительных шаблонов до и после таблицы
+    before.reverse().forEach(subName => {                            
+        root[subName] = cloneTemplate(subName);            
+        root.container.append(root[subName].container);    
+    });
+    after.forEach(subName => {                            
+        root[subName] = cloneTemplate(subName);            
+        root.container.append(root[subName].container);    
+    });
 
-    // @todo: #1.3 —  обработать события и вызвать onAction()
+    // обработка событии и вызов onAction()
+    root.container.addEventListener('chang', () => {
+        onAction();
+    });
+    root.container.addEventListener('reset', () => {
+        setTimeout(onAction, 1000);
+    });
+    root.container.addEventListener('submit', (e) => {
+        e.preventDefault();
+        onAction(e.submitter);
+    });
 
     const render = (data) => {
-        // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        // преобразование данных в массив строк на основе шаблона rowTemplate
+        const nextRows = data.map(item => { 
+            const row = cloneTemplate(rowTemplate);
+            Object.keys(item).forEach(key => {
+                if (key in row.elements) {
+                    row.elements[key].textContent = item[key];
+                }
+            });
+            return row.container;
+        });
         root.elements.rows.replaceChildren(...nextRows);
     }
 
