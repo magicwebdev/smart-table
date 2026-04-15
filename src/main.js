73358f4c1655dbd740338569536c6034e5708da5
Александра Храпкова <magicwebdev@yandex.ru>
2026-04-15 20:24:58 +0300

    добавляет правки по 7 проектной работе

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import './fonts/ys-display/fonts.css'
 import './style.css'
 
-import {data as sourceData} from "./data/dataset_1.js";
 import {initData} from "./data.js";
 import {processFormData} from "./lib/utils.js";
 import {initTable} from "./components/table.js";
@@ -10,7 +9,7 @@ import {initSorting} from './components/sorting.js';
 import {initFiltering} from './components/filtering.js';
 import {initSearching} from './components/searching.js';
 
-const api = initData(sourceData);
+const api = initData();
 
 /**
  * Сбор и обработка полей из таблицы
